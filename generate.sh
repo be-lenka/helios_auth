0e2a6f477453e84e04359a5d7b1b158b7f534bae
Peter Veres <peter.veres@gmail.com>
2023-10-16 12:25:07 +0200

    update composer

--- generate.sh
+++ generate.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/auth.json -g php -o /local/ -p developerOrganization=belenka -p composerPackageName=belenka/helios_auth -p invokerPackage=BeLenka\\Helios\\Auth
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/auth.json -g php -o /local/ -p licenseName="MIT" -p developerOrganization="BeLenka Dev" -p developerOrganizationUrl="https://www.belenka.com" -p composerPackageName=be-lenka/helios_auth -p invokerPackage=BeLenka\\Helios\\Auth
+
