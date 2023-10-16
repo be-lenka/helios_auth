c12579a852a82135efaa2d6af9f63a396dce8199
Peter Veres <peter.veres@gmail.com>
2023-10-16 12:40:34 +0200

    update dependencies

---
+++ generate.md
@@ -0,0 +1,4 @@
+download `auth.json` file available at:
+http://62.197.244.158:8080/doc/auth
+
+run `./generate.sh`
--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/auth.json -g php -o /local/ -p licenseName="MIT" -p developerOrganization="BeLenka Dev" -p developerOrganizationUrl="https://www.belenka.com" -p composerPackageName=be-lenka/helios_auth -p invokerPackage=BeLenka\\Helios\\Auth
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -t /local/resources -i /local/auth.json -g php -o /local/ -p licenseName="MIT" -p developerOrganization="BeLenka Dev" -p developerOrganizationUrl="https://www.belenka.com" -p composerPackageName=be-lenka/helios_auth -p invokerPackage=BeLenka\\Helios\\Auth --global-property appDescription="Helios Auth Package"
 
