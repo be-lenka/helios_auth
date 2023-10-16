835f37647718089e0f37683a984290199127cc04
Peter Veres <peter.veres@gmail.com>
2023-10-16 15:05:58 +0200

    fix generator version

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --user $(id -u):$(id -g) --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -t /local/resources -i /local/auth.json -g php -o /local/ -p licenseName="MIT" -p developerOrganization="BeLenka Dev" -p developerOrganizationUrl="https://www.belenka.com" -p composerPackageName=be-lenka/helios_auth -p invokerPackage=BeLenka\\Helios\\Auth
+docker run --user $(id -u):$(id -g) --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v7.0.0 generate -t /local/resources -i /local/auth.json -g php -o /local/ -p licenseName="MIT" -p developerOrganization="BeLenka Dev" -p developerOrganizationUrl="https://www.belenka.com" -p composerPackageName=be-lenka/helios_auth -p invokerPackage=BeLenka\\Helios\\Auth
 
 find "lib/" -type f -name '*.php' -print0 | while IFS= read -r -d '' file
 do
